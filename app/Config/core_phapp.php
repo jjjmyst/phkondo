5c0661994ea0d2b8d68ac0b44d91b40494466617
jjjmyst <47152872+jjjmyst@users.noreply.github.com>
2022-09-15 14:37:04 -0700

    Update and rename core_phapp.php.default to core_phapp.php

--- app/Config/core_phapp.php
+++ app/Config/core_phapp.php
@@ -35,7 +35,7 @@
  * In production mode, flash messages redirect after a time interval.
  * In development mode, you need to click the flash message to continue.
  */
-Configure::write('debug',0);
+Configure::write('debug',1);
 Configure::write('Exception.handler', 'AppExceptionHandler::handleException');
 Configure::write('Session', array('defaults' => 'php','cookie' => 'pHKondoSession'));
 Configure::write('Cookie', array('name' => 'pHKondoCookie'));
